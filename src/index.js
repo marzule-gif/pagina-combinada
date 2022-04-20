acab22095f49240f3a2f83c7774e3bcf65bb191e
marzule-gif <78831717+marzule-gif@users.noreply.github.com>
2022-04-20 10:56:31 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,138 @@
+const express = require("express");
+const path = require("path");
+const mysql = require("mysql")
+require('dotenv').config()
+
+var hist
+let fecha_in=0
+let fecha_fin=0
+let fecha=0
+let press_lat=0
+let press_lng=0
+var ruta
+
+const dgram = require('dgram');
+const req = require("express/lib/request");
+const { timeStamp } = require("console");
+const server = dgram.createSocket('udp4');
+
+const data = {
+  lat: "",
+  long: "",
+  time: "",
+  date: "",
+}
+
+var con = mysql.createConnection({  
+  host: process.env.HOST,  
+  user: process.env.USER_DB,  
+  password: process.env.PASSWORD,  
+  database: process.env.DB  
+});  
+con.connect(function(err) {  
+if (err) throw err;  
+console.log("Connected!");})  
+
+const app = express();
+app.use(express.static(path.join(__dirname , "public")));
+app.use(express.json())
+
+app.get("/", (req, res) => {
+  console.log(true)
+  res.sendFile(path.join(__dirname + "/public/main.html"));
+});
+
+app.get('/data',(req,res)=>{
+  con.query('select * from datos ORDER BY id DESC LIMIT 1',(err,message)=>{
+   
+     res.status(200).json({
+      data: message
+      
+      
+    });
+    
+  });
+});
+
+
+server.on('error', (err) => {
+  console.log(`server error:\n${err.stack}`);
+  server.close();
+});
+server.on('message', async (msg, senderInfo) => {
+  console.log('Messages received ' + msg)
+  const mensaje = String(msg).split("\n")
+
+  const variables = 
+  data.lat = mensaje[0].split(" ")[1]
+  data.long = mensaje[1].split(" ")[1]
+  data.time = mensaje[2].split(" ")[2];
+  data.date = mensaje[2].split(" ")[1].slice(1)
+  fecha = data.date+" "+data.time;
+
+  var ts = new Date(fecha).getTime();
+  console.log(ts);
+
+  
+
+  var sql = `INSERT INTO datos (latitud , longitud, hora, fecha,timestamp) VALUES ('${data.lat}','${data.long}','${data.time}','${data.date}','${ts}')`;
+  con.query(sql, function (err, result) {  
+    if (err) throw err;  
+    console.log("dato recibido");  
+    });  
+});
+server.on('listening', (req, res) => {
+  const address = server.address();
+  console.log(`server listening on ${address.address}:${address.port}`);
+});
+
+app.post("/historicos", function(req, res) {
+  
+    hist = req.body;
+    fecha_in= hist[0];
+    fecha_fin=hist[1];
+    
+});
+
+app.get('/request',(req,res)=>{
+
+    con.query(`select latitud,longitud, fecha, hora from datos where timestamp between'${fecha_in}' and '${fecha_fin}' 
+      order by id`,(err, historial,fields)=>{
+      
+      res.status(200).json({
+        data: historial,
+      });
+    })
+
+})
+
+app.post("/update", function(req, res) {
+  console.log("Actualizado")
+
+})
+
+app.post("/rutas", function(req, res) {
+  
+  ruta = req.body;
+  press_lat=ruta.lat;
+  press_lng=ruta.lng;
+
+  console.log(press_lat)
+  console.log(press_lng)
+
+  var rutas = con.query(`select * from datos where (latitud-'${press_lat}')*(latitud-'${press_lat}')+(longitud-('${press_lng}'))*(longitud-('${press_lng}'))<0.0001 order by id`,
+  (err, historial,fields)=>{
+    
+    console.log(historial)
+    res.json(historial)
+  })
+
+});
+
+
+
+server.bind(3020);
+
+app.listen(3000,()=>{
+    console.log(`server connected http://localhost:${3000}`)
+});
---
+++ src/public/scripts/scripts.js
@@ -0,0 +1,157 @@
+let marcador = null;
+let marcador2 = null;
+let polyline = null;
+let polyline2 = null;
+let polyline3 = null;
+let start = 0
+let end = 0
+let circle = null;
+var Vector_r = null;
+var slider = document.getElementById("slider");
+const byplce = document.querySelector('.byplace');
+let datos = null;
+var cirlceMarkers;
+
+const map = L.map('mapa');
+map.setView([10.9767610, -74.8307289], 12);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+const long = document.getElementById("long");
+const lat = document.getElementById("lat");
+const time = document.getElementById("time");
+const date = document.getElementById("date");
+
+var greenIcon = new L.Icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+  });
+
+document.getElementById("start").addEventListener("change", function() {
+    var inicio = this.value;
+    start = new Date(inicio).getTime();
+    document.getElementById("stop").min = this.value
+})
+
+document.getElementById("stop").addEventListener("change", function() {
+    var fin = this.value
+    end = new Date(fin).getTime();
+    document.getElementById("start").max = this.value
+})
+
+const button = document.getElementById("button");
+button.addEventListener("click",function(){
+
+    var fechas =[start,end];
+    console.log(fechas)
+    fetch('/historicos' ,{
+        headers:{
+            'Content-type':'application/json'
+        },
+        method: 'post',
+        body: JSON.stringify(fechas)
+    }).then(res => res.json())
+
+    getFecha();
+})
+
+
+let array = [];
+
+async function getData() {
+    const res = await fetch('/data')
+    let json = await res.json()
+
+    const {latitud, longitud, hora, fecha} = json.data[0];
+    long.innerHTML=longitud;
+    lat.innerHTML=latitud;
+    time.innerHTML=hora;
+    date.innerHTML=fecha;
+
+    let LatLng = new L.LatLng(latitud, longitud)
+    array.push(LatLng);
+
+    if (marcador) marcador.setLatLng(LatLng)
+    else marcador = L.marker(LatLng,{icon: greenIcon}).bindPopup('Usted está aquí').addTo(map)
+
+    if (polyline) {
+        polyline.setLatLngs(array)
+    }else {
+        polyline = L.polyline(array, {color: 'aqua'}).addTo(map)
+    }
+} 
+
+setInterval(getData,5000);
+
+async function getFecha() {
+    const res = await fetch('/request')
+    let json = await res.json()
+
+    datos = json.data;
+
+    var Nvector = [];
+    
+    for (var i = 0, max = datos.length; i < max; i+=1) {
+ 
+        Nvector.push([datos[i].latitud,datos[i].longitud]);
+    }
+     polyline2 = L.polyline(Nvector, {color: 'blue'}).addTo(map)
+}
+
+let popup = L.popup();
+    
+slider.onchange = e => {
+    if(cirlceMarkers){
+        map.removeLayer(cirlceMarkers)
+    }
+
+    const center = [datos[slider.value].latitud, datos[slider.value].longitud]
+    Vector_r = center
+    console.log(Vector_r)
+    if (circle) circle.setLatLng(Vector_r)
+    else  circle=L.circle(Vector_r, {radius: 150}).addTo(map);
+
+    const marker2 = L.marker(center).addTo(map)
+    cirlceMarkers = marker2
+
+    const formatCenter = circle.getLatLng()
+
+    var cont = 0;
+    byplce.innerHTML = null;
+    for(var marker of datos){
+        if(Math.abs(formatCenter.distanceTo([marker.latitud, marker.longitud])) < 150){
+            const hist_item = create_hist_item(marker, cont);
+            byplce.appendChild(hist_item)
+            cont++
+        }
+    }
+}
+
+function create_hist_item(object, id) {
+    const place_item = document.createElement('div');
+    place_item.classList.add('place-item');
+
+        const index = document.createElement('div');
+        index.classList.add('index');
+        index.innerHTML = id;
+
+        const place_content = document.createElement('div');
+        place_content.classList.add('place-item-content');
+
+            const place_coords = document.createElement('div');
+            place_coords.classList.add('place-item-coords');
+            place_coords.innerHTML = `${object.latitud}, ${object.longitud}`;
+
+            const place_time = document.createElement('div');
+            place_time.classList.add('place-item-time');
+            place_time.innerHTML = `${object.fecha} : ${object.hora}`;
+        place_content.appendChild(place_coords);
+        place_content.appendChild(place_time);
+    place_item.appendChild(index);
+    place_item.appendChild(place_content);
+
+    return place_item;
+}
